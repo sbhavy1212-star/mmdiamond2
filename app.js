a1d663d556700cf5799829b791342f36f975a179
Bhavy <s.bhavy1212@gmail.com>
2026-06-25 17:12:44 +0530

    Week 2 - JWT Authentication completed

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const app = express();
+const authRoutes =require("./routes/authRoutes");
+const auth = require("./middleware/authMiddleware");
 
 connectDB();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth",authRoutes);
+
+app.get("/api/protected", auth, (req, res) => {
+
+    res.json({
+        message: "Protected Route Accessed",
+        user: req.user
+    });
+
+});
 
 app.get("/", (req, res) => {
     res.send("M & M Diamond API Running");
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+
+    try {
+
+        const token =
+            req.header("Authorization")
+            ?.replace("Bearer ", "");
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Access Denied"
+            });
+        }
+
+        const verified = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        req.user = verified;
+
+        next();
+
+    } catch (error) {
+
+        res.status(401).json({
+            message: "Invalid Token"
+        });
+    }
+};
